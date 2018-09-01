4e473048841997380d8393c252455aca844b86ab
Rokas <ktu.vaizgantas@gmail.com>
2018-09-01 12:24:21 +0300

    heroku

--- app.js
+++ app.js
@@ -4,8 +4,11 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 let logger = require('morgan');
+const fs = require('fs');
 
-let indexRouter = require('./routes/index');
+const users = require('./client/src/Data/Users.json');
+const books = require('./client/src/Data/BookData.json');
+const subjects = require('./client/src/Data/Subjects.json');
 
 let app = express();
 
@@ -32,7 +35,79 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.resolve(__dirname, './client/build')));
 
-app.use('/', indexRouter);
+app.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+app.get('/api/books', (req, res) => {
+  res.json(books);
+});
+
+app.get('/api/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/api/subjects', (req, res) => {
+  res.json(subjects);
+});
+
+app.post('/api/books', (req, res) => {
+  fs.writeFile('./client/src/Data/BookData.json', JSON.stringify(req.body),'utf-8', (err) => {
+    if (err) throw err;
+  });
+
+  res.end('Success');
+});
+
+app.post('/login', (req, res, next) => {
+  fs.readFile('./client/src/Data/Users.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    let users = JSON.parse(data);
+    let newLogin = req.body;
+    let loggedUser = {};
+    let errors = '';
+    const user = users.filter(user => user.email === newLogin.email);
+
+    if (user.length === 0 || (user.length > 0 && user[0].password !== newLogin.password)) {
+      errors = 'Email address or password was invalid';
+    } else {
+      loggedUser = user[0];
+    }
+    res.json({ errors, loggedUser });
+  });
+});
+
+app.post('/register', (req, res) => {
+  fs.readFile('./client/src/Data/Users.json', 'utf-8', (err, data) => {
+    if (err) throw err;
+
+    let users = JSON.parse(data);
+    let newRegister = req.body;
+
+    let errors = [];
+
+    for (let i = 0; i < users.length; i++) { //Check if username is unique
+      if (users[i].userName === newRegister.userName) {
+        errors.push('User name must be unique!');
+      }
+      if (users[i].email === newRegister.email) {
+        errors.push('Email address must be unique!');
+      }
+    }
+
+    if (errors.length === 0) {
+      newRegister = { id: users.length + 1, ...newRegister };
+      users.push(newRegister);
+
+      fs.writeFile('./client/src/Data/Users.json', JSON.stringify(users),'utf-8', (err) => {
+        if (err) throw err;
+      });
+    }
+
+    res.json(errors);
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
